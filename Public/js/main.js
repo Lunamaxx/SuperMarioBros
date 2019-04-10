c9751cca6a7f5838cb3f8cf7a1f7f1255e2f4f5d
lunamaxx <jonboy1023@gmail.com>
2019-04-10 10:59:49 -0400

    adding new files

---
+++ Public/js/main.js
@@ -0,0 +1,40 @@
+import SpriteSheet from './SpriteSheet.js';
+import {loadImage, loadLevel} from './loaders.js';
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
+    });
+}
+
+
+
+
+function loadBackgroundSprites(){
+    return loadImage('/img/tiles.png')
+    .then(image => {
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.defineTile('ground', 0, 0);
+        sprites.defineTile('sky', 3, 23);
+        return sprites;
+    });
+}
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+    //loadMarioSprite(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([/*marioSprite,*/ sprites, level]) => {
+    level.backgrounds.forEach(background => {
+        drawBackground(background, context, sprites);
+    });
+
+    marioSprite.draw('idle', context, 64, 64);
+});
