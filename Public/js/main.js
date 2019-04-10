83dd3eacce2a15f0c410b71efc21806f1d0555bf
lunamaxx <jonboy1023@gmail.com>
2019-04-10 16:54:32 -0400

    pushed during vid 5 at 28 mins

--- Public/js/main.js
+++ Public/js/main.js
@@ -2,29 +2,24 @@ import Compositor from './Compositor.js';
 import Timer from './Timer.js';
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import{createBackgroundLayer, createSpriteLayer} from './layers.js';
 
 import Keyboard from './keyboardState.js';
 
-
+//pushed at 28:50 in vid
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
     createMario(),
-    loadBackgroundSprites(),
     loadLevel('1-1'),
 ])
-.then(([mario, backgroundSprites, level]) => {
-    const comp = new Compositor();
-
-    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-    comp.layers.push(backgroundLayer);
+.then(([mario, level]) => {
     
     const gravity = 2000;
-    mario.pos.set(64,180);
+    mario.pos.set(64,64);
+
+    level.entities.add(mario);
     
 
     const SPACE = 32;
@@ -39,14 +34,13 @@ Promise.all([
     });
     input.listenTo(window);
 
-    
-    const spriteLayer = createSpriteLayer(mario);
-    comp.layers.push(spriteLayer);
 
     const timer = new Timer(1/60);
     timer.update = function update(deltaTime){
-        mario.update(deltaTime);
-        comp.draw(context);
+        level.update(deltaTime);
+
+        level.comp.draw(context);
+
         mario.vel.y += gravity * deltaTime;
     }
 
